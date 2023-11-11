2e6632c4b57ec7941ed8806a33291e57032b9455
youssef ouakrim <youssefouakrim39@gmail.com>
2023-11-11 10:40:46 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "httphandler.youssefouakrim39.workers.dev",
+  VITE_TMDB_READ_API_KEY: "272c720139b01500699e0ac292db04cb"
+};
